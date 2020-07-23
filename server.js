747e99cdea58e93644a006f3ad438abeb3bc167c
thomasfregene <fregzythomas@gmail.com>
2020-07-23 02:01:05 +0100

    Created register and signin api -smart-brain-server

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res)=>{
+    res.send(database.users)
+})
+
+//signin
+app.post('/signin', (req, res)=>{
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password){
+            res.json('success');
+        }else{
+            res.status(400).json('error logging in');
+        }
+})
+
+//register
+app.post('/register', (req, res)=>{
+    const{email, name, password} = req.body;
+    database.users.push({
+        id: '124',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.listen(4000, ()=>{
+    console.log('app is running on port 4000');
+})
