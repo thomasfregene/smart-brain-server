f6731dc54fafad8f55d3bb89fcea8248a8ca44b5
thomasfregene <fregzythomas@gmail.com>
2020-07-24 19:56:03 +0100

    Integrated the register and sihnin action with Database -postgre

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
-const cors = require('cors');
-const knex = require('knex');
+const cors = require("cors");
+const knex = require("knex");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '   ',
-    database : 'smartbrain'
-  }
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "   ",
+    database: "smartbrain",
+  },
 });
-
 
 const app = express();
 
@@ -21,67 +20,89 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
   res.send(database.users);
-})
+});
 
 //signin
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
-})
+  db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data=>{
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if(isValid){
+      return db.select('*').from('users')
+      .where('email', '=', req.body.email)
+      .then(user=>{
+        res.json(user[0])
+      })
+      .catch(err=>res.status(400).json('unable to get user'))
+    }else{
+      res.status(400).json('wrong credentials')
+    }
+    
+  })
+  .catch(err=>res.status(400).json('wrong credentials'))
+});
 
 //register
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-  .returning('*')
-  .insert({
-    email: email,
-    name: name,
-    joined: new Date
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
   })
-  .then(user=>{
-    res.json(user[0]);
-  })
-  .catch(err=>res.status(400).json('Unable to register'))
-    
+  .catch((err) => res.status(400).json("Unable to register"));
 });
 
 //get by id
 app.get("/profile/:id", (req, res) => {
   //destructuring props
   const { id } = req.params;
-  db.select('*').from('users').where({id})
-  .then(user=>{
-    if(user.length){
-      res.json(user[0]);
-    }else{
-      res.status(400).json('Not Found')
-    }
-  })
-  .catch(err=>res.status(400).json('error getting user'))
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not Found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
 });
 
-app.put('/image', (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment( 'entries', 1)
-  .returning('entries')
-  .then(entries=>{
-    res.json(entries[0]);
-  })
-  .catch(err=>res.status(400).json('unsable to get entries'))
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unsable to get entries"));
 });
-
-
 
 app.listen(4000, () => {
   console.log("app is running on port 4000");
