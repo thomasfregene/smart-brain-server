18cc63d1b971a48f2fcc83edf2acaec87f47cf5a
thomasfregene <fregzythomas@gmail.com>
2020-07-25 15:34:07 +0100

    final commit

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.get("/profile/:id", (req, res)=>{profile.handleProfileGet(req, res, db)});
 app.put("/image",(req, res)=>{image.handleImage(req, res, db)});
 app.post("/imageUrl",(req, res)=>{image.handleApiCall(req, res)});
 
+
 app.listen(4000, () => {
-  console.log("app is running on port 4000");
+  console.log('app is running on port 4000');
 });
